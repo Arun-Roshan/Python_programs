e279343bb09f0016fb41d2e31ff6d927fa0bbb63
Arun-Roshan <83696282+Arun-Roshan@users.noreply.github.com>
2022-07-15 10:57:25 +0530

    calculates the highest score
    a program that calculates the highest score from a List of scores.

---
+++ maximum score.py
@@ -0,0 +1,10 @@
+# a program that calculates the highest score from a List of scores.
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+high_score = 0
+for m in student_scores:
+    if m > high_score:
+        high_score = m
+print(f"The highest score in the class is: {high_score}")
