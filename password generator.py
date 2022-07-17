93e72094f87d66fd2d225b85fa850acfad32429f
Arun-Roshan <83696282+Arun-Roshan@users.noreply.github.com>
2022-07-17 16:06:13 +0530

    password generator
    generator ur password

---
+++ password generator.py
@@ -0,0 +1,29 @@
+# password generator
+import random
+
+letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+          "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+          "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+print("Welcome to the Py password generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like in your password?\n"))
+nr_numbers = int(input("How many numbers would you like in your password?\n"))
+
+password = ""
+
+for l in range(1, nr_letters + 1):
+    random_char = random.choice(letter)
+    password += random_char
+
+
+for n in range(1, nr_numbers + 1):
+    random_number = random.choice(numbers)
+    password += random_number
+
+for s in range(1, nr_symbols + 1):
+    random_symbols = random.choice(symbols)
+    password += random_symbols
+print(password)
